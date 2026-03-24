3a7f8282982e2ec706fcdc9d9a961e7c9e91cf87
Khwaaish Patwa <khwaaishpatwa@gmail.com>
2026-03-24 21:08:23 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    if (n == 0) {
+        cout << 0;
+        return 0;
+    }
+    if (n == 1) {
+        cout << 1;
+        return 0;
+    }
+
+    int a = 0, b = 1, c;
+
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    cout << b;
+
+    return 0;
+}
