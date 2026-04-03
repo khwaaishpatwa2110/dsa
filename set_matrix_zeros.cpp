b034d4acbcd4a2e5ffedf997a056f89d6462cc5d
Khwaaish Patwa <khwaaishpatwa@gmail.com>
2026-04-03 22:36:48 +0530

    Add files via upload

---
+++ set_matrix_zeros.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+void setZeros(int matrix[][100], int m, int n) {
+    int firstRowZero = 0;
+    int firstColZero = 0;
+
+    // first row
+    for (int j = 0; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            firstRowZero = 1;
+            break;
+        }
+    }
+
+    // first column
+    for (int i = 0; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            firstColZero = 1;
+            break;
+        }
+    }
+
+    // mark rows and columns
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // setting zeros based on markers
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // first row
+    if (firstRowZero == 1) {
+        for (int j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // first column
+    if (firstColZero == 1) {
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}
+
+int main() {
+    int m, n;
+    cout << "Enter number of rows: ";
+    cin >> m;
+    cout << "Enter number of columns: ";
+    cin >> n;
+
+    int matrix[100][100];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    setZeros(matrix, m, n);
+
+    cout << "Output matrix:\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
